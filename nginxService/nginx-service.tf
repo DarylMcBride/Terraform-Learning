5e5220d96915c5c54660febf4e84948ba2e1b310
daryl <daryl332@hotmail.com>
2019-12-10 11:18:55 +0000

    adding nginx service

---
+++ nginxService/nginx-service.tf
@@ -0,0 +1,43 @@
+resource "kubernetes_service" "nginx_service" {
+	metadata {
+		name = "nginx_service"
+	}
+	spec {
+		selector {
+			app = "${kubernetes_pod.nginx_pod.metadata.0.labels.app}"
+		}
+		port {
+			name = "https"
+			port = 443
+			target_port = 443
+		}
+		port {
+			name = "http"
+			port = 80
+			target_port = 80
+		}
+		type = "LoadBalancer"
+	}
+}
+
+resource "kubernetes_pod" "nginx_pod" {
+	metadata {
+		name = "nginx-pod"
+		labels {
+			app = "nginx-pod"
+		}
+	}
+
+	spec {
+		container {
+			image = "nginx:latest"
+			name = "nginx"
+			port {
+				container_port = 80
+				container_port = 443
+			}
+		}
+	}
+}
+
+
